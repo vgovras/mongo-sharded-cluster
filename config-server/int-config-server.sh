b36b816137d6d7ec6ca2f3aa5aaf87ef745948c1
Vyacheslav <govras99@sharkscode.com>
2022-10-30 21:49:47 +0200

    fix errors with port on config server

--- config-server/int-config-server.sh
+++ config-server/int-config-server.sh
@@ -1,4 +1,4 @@
-mongosh --host ${CFG_HOST_0}:27017 << EOF
-    var config = { "_id": "$CFG_RS", "members": [{ "_id": 0, "host": "${CFG_HOST_0}:27017" }, { "_id": 1, "host": "${CFG_HOST_1}:27017" }, { "_id": 2, "host": "${CFG_HOST_2}:27017" }] };
+mongosh --host ${CFG_HOST_0}:${CFG_PORT} << EOF
+    var config = { "_id": "${CFG_RS}", "members": [{ "_id": 0, "host": "${CFG_HOST_0}:${CFG_PORT}" }, { "_id": 1, "host": "${CFG_HOST_1}:${CFG_PORT}" }, { "_id": 2, "host": "${CFG_HOST_2}:${CFG_PORT}" }] };
     rs.initiate(config);
 EOF
